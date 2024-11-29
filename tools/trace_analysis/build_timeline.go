77ec804040456e871e5aea2e1d8bc55b73d5f97a
Leonid Kondrashov <leo.kondrashov@gmail.com>
2024-11-29 16:34:16 +0800

    Create compressed timeline
    Signed-off-by: Leonid Kondrashov <leo.kondrashov@gmail.com>

--- tools/trace_analysis/build_timeline.go
+++ tools/trace_analysis/build_timeline.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"slices"
 	"time"
 
 	"github.com/vhive-serverless/loader/pkg/common"
 )
+
+type TimelineEntry struct {
+	Timestamp   float64
+	Concurrency int
+}
 
 func generateFunctionTimeline(function *common.Function, duration int, granularity time.Duration) []int {
 	minuteIndex, invocationIndex := 0, 0
@@ -45,4 +51,69 @@ func generateFunctionTimeline(function *common.Function, duration int, granulari
 	}
 
 	return concurrency
+}
+
+func totalInvocations(IAT common.IATMatrix) int {
+	total := 0
+	for _, minute := range IAT {
+		total += len(minute)
+	}
+	return total
+}
+
+func generateFunctionTimelineCompressed(function *common.Function, duration int) []TimelineEntry {
+	minuteIndex, invocationIndex := 0, 0
+	sum := 0.0
+
+	IAT, runtimeSpecification := function.Specification.IAT, function.Specification.RuntimeSpecification
+	timeline := make([]TimelineEntry, 0, 2*totalInvocations(IAT))
+
+	for {
+		if minuteIndex >= duration {
+			break
+		} else if function.InvocationStats.Invocations[minuteIndex] == 0 {
+			minuteIndex++
+			invocationIndex = 0
+			sum = 0.0
+			continue
+		}
+
+		sum += IAT[minuteIndex][invocationIndex] / float64(time.Second/time.Microsecond)
+
+		duration := float64(runtimeSpecification[minuteIndex][invocationIndex].Runtime) / float64(time.Second/time.Millisecond)
+		startTime := float64(minuteIndex*int(time.Minute/time.Second)) + sum
+		timeline = append(timeline, TimelineEntry{
+			Timestamp:   startTime,
+			Concurrency: 1,
+		})
+		timeline = append(timeline, TimelineEntry{
+			Timestamp:   startTime + duration,
+			Concurrency: -1,
+		})
+
+		invocationIndex++
+		if function.InvocationStats.Invocations[minuteIndex] == invocationIndex {
+			minuteIndex++
+			invocationIndex = 0
+			sum = 0.0
+		}
+	}
+
+	slices.SortFunc(timeline, func(i, j TimelineEntry) int {
+		if i.Timestamp < j.Timestamp {
+			return -1
+		} else if i.Timestamp > j.Timestamp {
+			return 1
+		} else {
+			return i.Concurrency - j.Concurrency
+		}
+	})
+
+	concurrency := 0
+	for i := 0; i < len(timeline); i++ {
+		concurrency += timeline[i].Concurrency
+		timeline[i].Concurrency = concurrency
+	}
+
+	return timeline
 }
