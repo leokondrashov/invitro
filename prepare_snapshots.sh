b22af8d1168d3ad00380089ab2371ccc2c79bede
Leonid Kondrashov <leo.kondrashov@gmail.com>
2026-03-23 00:16:47 -0400

    collect ws over several invocations in prepare snapshots
    Signed-off-by: Leonid Kondrashov <leo.kondrashov@gmail.com>

--- prepare_snapshots.sh
+++ prepare_snapshots.sh
@@ -6,12 +6,12 @@ RELAY_ARGS=$2
 mc ls minio/snapshots
 
 reset_relay() {
-	for n in 3
+	for n in {3..10}
 	do
 		ssh 10.0.1.$n "tmux send-keys -t relay C-c"
 	done
     sleep 5
-	for n in 3
+	for n in {3..10}
     do
     	ssh 10.0.1.$n "tmux send -t relay 'sudo ./relay -ss proxy -snapshots remote -dbg -netPoolSize 1 -endpoint 10.0.1.$n:8080 $RELAY_ARGS 2>&1 > ~/relay.log' ENTER"
     done
@@ -19,7 +19,7 @@ reset_relay() {
 }
 
 get_relay_logs() {
-    for n in 3
+    for n in {3..10}
     do
         scp 10.0.1.$n:~/relay.log data/out/relay_$n.log
     done
@@ -43,7 +43,7 @@ sed 's/"TracePath": ".*",/"TracePath": "'$TRACE'",/' cmd/config_warming_up.json 
 
 go run cmd/loader.go -config cmd/config_warming_up_tmp.json -verbosity debug -justDeploy
 
-for n in 1 2 3; do # go over three times to make sure there are all snapshot and working sets
+for n in 1 2 3 4 5 6; do # go over three times to make sure there are all snapshot and working sets
     for url in `kn service list | awk 'NR>1 {print $2}'`; do
         echo "Warming up functions at $url"
         # normalize url: strip protocol, everything after the first period, and after the last dash
@@ -51,10 +51,10 @@ for n in 1 2 3; do # go over three times to make sure there are all snapshot and
         name="${name%%.*}"
         name="${name%-*}"
 
-        if mc stat minio/snapshots/$name/working_set_pages >/dev/null 2>&1; then
-            echo "Skipping $name as snapshot is already available"
-            continue
-        fi
+        # if mc stat minio/snapshots/$name/working_set_pages >/dev/null 2>&1; then
+        #     echo "Skipping $name as snapshot is already available"
+        #     continue
+        # fi
         timeout 30 ../vhive/bin/grpcurl -import-path ../vhive/function-images/springboot/proto -proto helloworld.proto -plaintext -d '{"name": "record"}' ${url#*://}:80 helloworld.Greeter/SayHello 2>&1 > /dev/null || echo "Function $name did not respond"
     done
 done
