7af45c3b2e2d711f341f162796eb5b19fa2cb9f7
Leonid Kondrashov <leo.kondrashov@gmail.com>
2026-09-02 05:15:32 -0500

    oops
    Signed-off-by: Leonid Kondrashov <leo.kondrashov@gmail.com>

--- scripts/revert_kd.sh
+++ scripts/revert_kd.sh
@@ -24,7 +24,7 @@ sudo kubeadm upgrade apply v1.32.0 \
 '
 
 for node in $(kubectl get nodes -o jsonpath='{.items[*].metadata.name}'); do
-    if [[ $node =~ fake ]] && continue
+    [[ $node =~ fake ]] && continue
     echo "Restoring kubelet on $node"
 
     ssh $node sudo chmod 777 /tmp/kubelet.stock
